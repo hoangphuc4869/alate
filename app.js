be8075e4757f2ed8afded014527bdd80fcdcc880
hoangphuc4869 <phuchoangdo4869@gmail.com>
2023-11-06 11:43:28 +0700

    update

--- app.js
+++ app.js
@@ -40,3 +40,42 @@ var swiper = new Swiper(".ProjectSwiper", {
     clickable: true,
   },
 });
+
+const angleDowns = document.querySelectorAll(".tabs i");
+const list = document.querySelectorAll(".list-tab");
+var isArrowDown = new Array(angleDowns.length).fill(true);
+
+if (angleDowns.length > 0 && list.length > 0) {
+  for (var i = 0; i < list.length; i++) {
+    list[i].classList.add("hide");
+    angleDowns[i].classList.remove("fa-minus");
+    angleDowns[i].classList.add("fa-plus");
+  }
+  list[0].classList.add("show");
+  angleDowns[0].classList.remove("fa-plus");
+  angleDowns[0].classList.add("fa-minus");
+}
+
+if (angleDowns) {
+  angleDowns.forEach((angle, index) => {
+    angle.addEventListener("click", () => {
+      list.forEach((tab, tabIndex) => {
+        if (tabIndex === index) {
+          tab.classList.toggle("show");
+        } else {
+          tab.classList.remove("show");
+        }
+      });
+
+      angleDowns.forEach((arrow, arrowIndex) => {
+        if (arrowIndex === index) {
+          arrow.classList.toggle("fa-minus");
+          arrow.classList.toggle("fa-plus");
+        } else {
+          arrow.classList.remove("fa-minus");
+          arrow.classList.add("fa-plus");
+        }
+      });
+    });
+  });
+}
